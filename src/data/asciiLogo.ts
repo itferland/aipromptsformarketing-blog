cc77894df70991832efdbb7a45040254853c72a2
itferland <itferland@gmail.com>
2025-07-03 17:49:46 -0400

    refactor(asciiLogo): replace array with a simple string for asciiLogo

--- src/data/asciiLogo.ts
+++ src/data/asciiLogo.ts
@@ -1,7 +1,2 @@
 /** String.raw keeps all back-ticks & escapes exactly as written */
-export const asciiLogo = [
-  ".xx;              :MWo",
-  "XMM0              :MMW",
-  "OMMx ,MMM  XMMc   :MMW  1 dXMM. .dXMMKW. ; .@W.MMMMW,",
-  "OMMx ,MMM  XMMc   :MMW  … (rest of your art, no escaping needed)"
-].join("\n");
+export const asciiLogo = "Test";
